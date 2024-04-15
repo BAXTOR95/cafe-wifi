2ce23adac180a489a36619f5acee459eaeac6e74
Brian Arriaga <brian.arriaga@gmail.com>
2024-04-15 16:57:33 -0400

    Add Google Maps API

--- web/routes.py
+++ web/routes.py
@@ -1,6 +1,14 @@
+import os
 from flask import Blueprint, render_template, request, redirect, url_for
 
 web_blueprint = Blueprint('web', __name__, template_folder='templates')
+
+
+# Context processor to inject the MAPS_API_KEY into all templates
+@web_blueprint.app_context_processor
+def inject_maps_key():
+    maps_api_key = os.getenv('MAPS_API_KEY')
+    return dict(MAPS_API_KEY=maps_api_key)
 
 
 @web_blueprint.route("/")
